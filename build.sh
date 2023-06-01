c7b4516fe09850a8886820d5824db079c4d8c4ae
Alex <burmenskiy90@bk.ru>
2023-06-01 17:56:27 +0300

    DB4 commit

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ python3.9 -m pip install -r requirements.txt
 echo "Make Migration..."
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
-
+venv\Scripts\activate
+  
 echo "Collect Static..."
 python3.9 manage.py collectstatic --noinput --clear
